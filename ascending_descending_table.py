9e58c51f11e258b5828dc5ba96072bc81f581616
sahill02 <sahilkhann019@gmail.com>
2026-08-29 12:40:18 +0530

    ascending_descending_table.py

---
+++ ascending_descending_table.py
@@ -0,0 +1,15 @@
+num = int(input("Enter the number which table you want to print "))
+choice = int(input("Do you want to print table ascending or descending order\nPress 0 for Desceding\nPress any Number for ascending"))
+
+print("your choice is", choice)
+
+if choice:
+    i =1
+    while i<=10:
+        print(f"{num} * {i} = {(num*i)}")
+        i+=1
+else:
+    i = 10
+    while i>=1:
+        print(f"{num} * {i} = {(num*i)}")
+        i-=1
